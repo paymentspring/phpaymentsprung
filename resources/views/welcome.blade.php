aa0eac7952aae0fea1a2ce390ec407fb503c0f57
Nathan Minchow <nathan.minchow@outlook.com>
2017-05-12 13:54:20 -0500

    Add links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello, world!</h1>
-</body>
-</html>
+@extends ('layout')
+
+@section ('content')
+    <h1>PhPaymentSprung</h1>
+    <a href="{{ url('customers') }}">Customers</a>
+    <a href="{{ url('customers/new') }}">Create a customer</a>
+@endsection
