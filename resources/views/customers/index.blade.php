83a2c5b7392ccb04569cc12a794e3c7758c01e07
Nathan Minchow <nathan.minchow@outlook.com>
2017-05-15 11:59:52 -0500

    Make customer index readable

--- resources/views/customers/index.blade.php
+++ resources/views/customers/index.blade.php
@@ -3,6 +3,19 @@
 @section ('content')
 
 <h1>Customers Index</h1>
-<p>{{ $body }}</p>
+@foreach ($body['list'] as $customer)
+  <div class="card">
+    ID: {{ $customer['id'] }} <br>
+    Company: {{$customer['company']}} <br>
+    Name: {{ $customer['first_name'] . " " . $customer['last_name'] }} <br>
+    Address:<br>
+    {{ $customer['address_1'] }} <br>
+    {{ $customer['city'] . ", " . $customer['state'] }} <br>
+    {{ $customer['zip'] }} <br>
+    Email: {{ $customer ['email'] }} <br>
+    Phone: {{ $customer ['phone'] }} <br>
+
+  </div>
+@endforeach
 
 @endsection
