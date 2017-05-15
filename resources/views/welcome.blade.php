b11eae9f94d664ec39d379f810408b85be00504f
Nathan Minchow <nathan.minchow@outlook.com>
2017-05-15 13:32:53 -0500

    Replace links with buttons on welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section ('content')
     <h1>PhPaymentSprung</h1>
-    <a href="{{ url('customers') }}">Customers</a>
-    <a href="{{ url('customers/new') }}">Create a customer</a>
-    <a href="{{ url('charges/card') }}">Charge a card</a>
+    <a href="{{ url('customers') }}" class="btn btn-default">Customers</a>
+    <a href="{{ url('customers/new') }}" class="btn btn-default">Create a customer</a>
+    <a href="{{ url('charges/card') }}" class="btn btn-default">Charge a card</a>
 @endsection
