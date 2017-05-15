2d5a30f8034d882cf370289cabeb1843247c65a4
Nathan Minchow <nathan.minchow@outlook.com>
2017-05-15 13:38:26 -0500

    Center content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,11 @@
 @extends ('layout')
 
 @section ('content')
-    <h1>PhPaymentSprung</h1>
-    <a href="{{ url('customers') }}" class="btn btn-default">Customers</a>
-    <a href="{{ url('customers/new') }}" class="btn btn-default">Create a customer</a>
-    <a href="{{ url('charges/card') }}" class="btn btn-default">Charge a card</a>
+    <div align="center">
+        <h1>PhPaymentSprung</h1>
+        <a href="{{ url('customers') }}" class="btn btn-default">Customers</a>
+        <a href="{{ url('customers/new') }}" class="btn btn-default">Create a customer</a>
+        <a href="{{ url('charges/card') }}" class="btn btn-default">Charge a card</a>
+    </div>
+
 @endsection
