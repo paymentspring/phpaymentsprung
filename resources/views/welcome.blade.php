14ba8b0dde212d1891ab8338e4b87b6c5eb40552
Nathan Minchow <nathan.minchow@outlook.com>
2017-05-15 13:39:02 -0500

    formatting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,5 +7,4 @@
         <a href="{{ url('customers/new') }}" class="btn btn-default">Create a customer</a>
         <a href="{{ url('charges/card') }}" class="btn btn-default">Charge a card</a>
     </div>
-
 @endsection
