b14e80a28d1e5cab9cba9742e1c14711359c9551
Nathan Minchow <nathan.minchow@outlook.com>
2017-05-15 14:27:05 -0500

    Reword customers button to "list customers"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section ('content')
     <div align="center">
         <h1>PhPaymentSprung</h1>
-        <a href="{{ url('customers') }}" class="btn btn-default">Customers</a>
+        <a href="{{ url('customers') }}" class="btn btn-default">List Customers</a>
         <a href="{{ url('customers/new') }}" class="btn btn-default">Create a customer</a>
         <a href="{{ url('charges/card') }}" class="btn btn-default">Charge a card</a>
     </div>
