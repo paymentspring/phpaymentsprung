2ce96c5080d327d1f1ffd6c97fd49d4f54379f4b
Nathan Minchow <nathan.minchow@outlook.com>
2017-05-15 11:59:30 -0500

    Add styling

--- resources/views/charges/card.blade.php
+++ resources/views/charges/card.blade.php
@@ -2,13 +2,13 @@
 
 @section ('content')
   <h1>Charge a card</h1>
-  <form action="card" method="post">
+  <form action="card" method="post" class="form-group">
     {{ csrf_field() }}
-    <input type="text" name="card_owner_name" placeholder="Card Owner Name">
-    <input type="text" name="card_number" placeholder="Card Number">
-    <input type="text" name="expiration_date" placeholder="MM/YYYY">
-    <input type="text" name="csc" placeholder="CSC">
-    <input type="text" name="amount" placeholder="00.00">
-    <input type="submit" value="submit">
+    <input type="text" name="card_owner_name" placeholder="Card Owner Name" class="form-control">
+    <input type="text" name="card_number" placeholder="Card Number" class="form-control">
+    <input type="text" name="expiration_date" placeholder="MM/YYYY" class="form-control">
+    <input type="text" name="csc" placeholder="CSC" class="form-control">
+    <input type="text" name="amount" placeholder="00.00" class="form-control">
+    <input type="submit" value="submit" class="btn btn-default">
   </form>
 @endsection
