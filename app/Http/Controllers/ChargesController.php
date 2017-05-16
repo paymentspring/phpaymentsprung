d5465e5a1bb5be191030ec57870182b8cc9258b8
Nathan Minchow <nathan.minchow@outlook.com>
2017-05-16 13:23:12 -0500

    add 'use ErrorException'

--- app/Http/Controllers/ChargesController.php
+++ app/Http/Controllers/ChargesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\TransferException;
+use ErrorException;
 
 class ChargesController extends Controller
 {
