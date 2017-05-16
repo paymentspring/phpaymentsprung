bae4830ebd16d69ad60e70583ab7f95d2500032d
Nathan Minchow <nathan.minchow@outlook.com>
2017-05-16 13:50:13 -0500

    Rename PhPaymentSpring to PHPaymentSpring

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section ('content')
     <div align="center">
-        <h1>PhPaymentSprung</h1>
+        <h1>PHPaymentSprung</h1>
         <a href="{{ url('customers') }}" class="btn btn-default">List Customers</a>
         <a href="{{ url('customers/new') }}" class="btn btn-default">Create a customer</a>
         <a href="{{ url('customers/search') }}" class="btn btn-default">Search for Customers</a>
