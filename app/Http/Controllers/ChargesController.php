f4ceaadc0b8ff7e0be3b0e3a371bf37dd4e717c6
Nathan Minchow <nathan.minchow@outlook.com>
2017-05-24 15:18:32 -0500

    Error handling in ChargesController

--- app/Http/Controllers/ChargesController.php
+++ app/Http/Controllers/ChargesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\TransferException;
+use Response;
 use ErrorException;
 
 class ChargesController extends Controller
@@ -12,6 +13,10 @@ class ChargesController extends Controller
     // Charges a card using a generated token
     public function chargeCard()
     {
+        // If the token was not created successfully, we grab the response here and send it back to the view
+        if (array_key_exists('errors', $_POST['params']['token'])) {
+            return Response::json(['code' => $_POST['params']['token']['errors'][0]['code'], 'message' => $_POST['params']['token']['errors'][0]['message']], 500);
+        }
         // Create body for charge
         $parameters = [
             "token" => $_POST['params']['token']['id'],
@@ -25,7 +30,8 @@ class ChargesController extends Controller
             $response = $client->post('https://api.paymentspring.com/api/v1/charge', [
                 'auth' => [env('PAYMENTSPRING_PRIVATE_KEY'), ''], 'body' => json_encode($parameters)]);
         } catch (TransferException $e) {
-            dd($e->getMessage());
+            // If guzzle has a problem posting the charge, we grab the error message and display it to the user
+            return Response::json(['code' => '', 'message' => $e->getMessage()], 500);
         }
         return $response->getBody()->getContents();
     }
